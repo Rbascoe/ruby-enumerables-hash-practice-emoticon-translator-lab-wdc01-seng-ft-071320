a02955690814432da8e92b273192b44c38c2e74c
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-09 22:28:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,9 @@ def get_english_meaning(file_path, emoticon)
     new_library[key][:japanese] == emoticon
   end
     emoticon
-   #  puts "Sorry, that emoticon was not found"
+  if emoticon != emoticon
+    puts "Sorry, that emoticon was not found"
+  end
 end
 
 def get_japanese_emoticon(file_path, emoticon)
